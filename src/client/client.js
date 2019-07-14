2127a10f5c6f7083027bb6636e18d4c926819c8f
Josh McKenney <joshmckenney@gmail.com>
2019-07-14 08:22:05 -0700

    Client setup for testing, modal available

--- src/client/client.js
+++ src/client/client.js
@@ -2,19 +2,23 @@ import { useState, useEffect } from "react";
 
 export const useFetch = (url, defaultData) => {
   const [data, updateData] = useState(defaultData);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
+      setIsLoading(true);
       if (!url) {
         updateData(defaultData);
+        setIsLoading(true);
         return;
       }
       const resp = await fetch(url);
       const json = await resp.json();
       updateData(json);
+      setIsLoading(true);
     }
     fetchData();
-  }, [url]);
+  });
 
   return data;
 };
